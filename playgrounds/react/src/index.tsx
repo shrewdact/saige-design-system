f43317caf28b893638efc0c174d9062cabc5fde1
성혁 주 <sh.ju@saigeresearch.ai>
2023-04-14 16:54:47 +0900

    style working on it

---
+++ playgrounds/react/src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { Button } from '@saige-research/design-system-react'
+
+
+ReactDOM.render(
+  <Button label="Saige Button"/>,
+  document.querySelector('#root')
+)
