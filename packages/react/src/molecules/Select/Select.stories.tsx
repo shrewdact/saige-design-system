ed515643af30d0c01b0c26cb50dec1282461a340
성혁 주 <sh.ju@saigeresearch.ai>
2023-04-17 22:58:40 +0900

    Setup storybook

---
+++ packages/react/src/molecules/Select/Select.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Select from './Select'
+
+// css
+import '@saige.ds/scss/lib/Select.css'
+
+const options = [
+  { label: 'Strict Black', value: 'black' },
+  { label: 'Heavenly Green', value: 'green' },
+  { label: 'Sweet Pink', value: 'pink' },
+]
+
+export default {
+  title: 'Select',
+}
+
+export const Common = () => <Select options={options} />
