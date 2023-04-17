1f6098dc63a143d957fca3e38bfd19a18093b042
성혁 주 <sh.ju@saigeresearch.ai>
2023-04-17 10:09:25 +0900

    Spacing component

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,5 +1,9 @@
 import Color from './atoms/Color'
+import Text from './atoms/Text'
+import Margin from './atoms/Margin'
 
 export {
   Color,
+  Text,
+  Margin
 }
--- playgrounds/react/src/index.tsx
+++ playgrounds/react/src/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { Color } from '@saige.ds/react'
+import { Text, Color, Margin } from '@saige.ds/react'
 
 import '@saige.ds/scss/lib/Utilities.css'
+// import '@saige.ds/scss/lib/Text.css'
+import '@saige.ds/scss/lib/Margin.css'
+import '@saige.ds/scss/lib/global.css'
 
 ReactDOM.render(
-  <Color hexCode='#000' width='lg' height='lg' />,
+  <Margin>
+    <Text size='xs'>this is some text</Text>
+  </Margin>,
+  // <Color hexCode='#000' width='lg' height='lg' />,
   document.querySelector('#root')
 )
